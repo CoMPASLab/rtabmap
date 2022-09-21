b3797626311bd65eba0933942890b1475980dc90
Tomi Laurinen <tomi.laurinen@gimrobotics.fi>
2022-09-21 14:38:46 +0300

    Add scripts for 3D visual version

---
+++ MBARI/visual-simulation0038.sh
@@ -0,0 +1,3 @@
+#!/bin/bash
+
+../build/bin/rtabmap-mbari-3d-mapping ~/mbari-datasets/SE/simulation_0038/ color/PROSILICA_L/ color/PROSILICA_R/ left_calib.yaml right_calib.yaml imu.csv imu_calib.yaml
